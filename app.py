206f4118a89b7f5d309f6bd18398bf7d5ee3b81e
KARINA-CUSTODIO <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-03-27 21:04:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,19 @@ planilha = api.open_by_key("1ZDyxhXlCtCjMbyKvYmMt_8jAKN5JSoZ7x3MqlnoyzAM")
 sheet = planilha.worksheet("Sheet1")
 app = Flask(__name__)
 
+
+def ultimas_promocoes():
+  scraper = ChannelScraper()
+  contador = 0
+  resultado = []
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    resultado.append(f"{message.created_at} {texto}")
+    if contador == 10:
+      return resultado
+
+
 menu = """
 <a href="/">Página inicial</a> | <a href="/promocoes">PROMOÇÕES</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
 <br>
@@ -32,7 +45,40 @@ def sobre():
 def contato():
   return menu + "Aqui vai o conteúdo da página Contato"
 
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
+  return conteudo + "</ul>"
 
+
+@app.route("/promocoes2")
+def promocoes2():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
+
+
+@app.route("/dedoduro2")
+def dedoduro2():
+  sheet.append_row(["Karina", "Custódio", "a partir do Flask"])
+  return "Planilha escrita!"
 
 
 
