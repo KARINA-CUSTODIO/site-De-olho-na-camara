941c0f6c8e01c79fdeb03c8ab5cdf90175ebd29a
Karina Custódio Sousa <114266179+KARINA-CUSTODIO@users.noreply.github.com>
2023-04-16 16:07:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ import os
 import gspread
 import requests
 from flask import Flask, request
+import gspread
+import zipfile
+import json
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
-import zipfile
-import altair as alt
-import plotly.express as px
 
-#Analisando gastos CEAP deputados Federais
+TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
+GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
+with open("credenciais.json", mode="w") as arquivo:
+  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+api = gspread.authorize(conta)
+planilha = api.open_by_key("1BTcO4G_FS1tp6_hRcPUk_4fts6ayt7Ms2cvYHsqD9nM")
+sheet = planilha.get_worksheet(3)
 
 #Criando função que baixa arquivo
 def baixar_arquivo(url, endereco):
@@ -25,36 +32,42 @@ def baixar_arquivo(url, endereco):
 #baixando arquivo despesas
 baixar_arquivo('https://www.camara.leg.br/cotas/Ano-2022.csv.zip','CSV')
 
-#lendo arquivo  
+with zipfile.ZipFile('CSV') as z:
+  print(z.namelist(),sep='\n')
+
 with zipfile.ZipFile('CSV') as z:
   with z.open('Ano-2022.csv') as f:
-    despesas = pd.read_csv(f, sep=';', skiprows = [i for i in range(1, 515) ])
+    despesas = pd.read_csv(f, sep=';', low_memory=False)
     
-#tratando dados
 despesas['numMes'] = despesas['numMes'].astype(int)
 
-#fazendo analises
 gastos = despesas['vlrLiquido'].sum()
 
 #Quais deputados mais gastaram?
 gastadores = despesas.groupby(['txNomeParlamentar', 'sgUF'])['vlrLiquido'].sum()
-gastadoresBR_top10 = gastadores.nlargest(10)
+gastadoresBR_top10 = gastadores.nlargest(10, keep='first')
 gastadoresBR_top10 = pd.DataFrame(gastadoresBR_top10)
 gastadoresBR_top10 = gastadoresBR_top10.reset_index()
+gastadoresBR_top10
+
+gastadores = despesas.groupby(['txNomeParlamentar', 'sgUF'])['vlrLiquido'].sum()
+gastadores = pd.DataFrame(gastadores)
+gastadores = gastadores.reset_index()
+
+#Levando dados do dataframe, pro Google sheets
+sheet_gastadores = planilha.get_worksheet(2)
+sheet_gastadores.update([gastadores.columns.values.tolist()] + gastadores.values.tolist())
 
 #Deputado/a que mais gastou
 maiorgastador = gastadoresBR_top10.iloc[0]['txNomeParlamentar']
 
-#Deputado/as que menos gastaram
+#Deputado/a que menos gastou
 gastadores = despesas.groupby(['txNomeParlamentar', 'sgUF'])['vlrLiquido'].sum()
 gastadores = pd.DataFrame(gastadores)
 gastadores = gastadores.sort_values(by='vlrLiquido')
 gastadores = gastadores.reset_index()
-menorgastador = gastadores.iloc[0]['txNomeParlamentar']
 
-#Qual a média de gastos por deputado/a?
-mediaBr = despesas['vlrLiquido'].mean()
-mediaBr = "{:.2f}".format(mediaBr)
+menorgastador = gastadores.iloc[0]['txNomeParlamentar']
 
 #Média de Gastos de deputados por estado
 estadosBr = despesas.groupby('sgUF')['vlrLiquido'].mean()
@@ -62,49 +75,28 @@ estadosBr.sort_values(ascending=False)
 estados = pd.DataFrame(estadosBr)
 estados = estados.reset_index()
 
-#Analisando Projetos de Lei
+#Analisando PLs
 
 baixar_arquivo('https://dadosabertos.camara.leg.br/arquivos/proposicoesAutores/csv/proposicoesAutores-2022.csv','proposicoesAutores-2022.csv')
 proposicoes = pd.read_csv('proposicoesAutores-2022.csv',
                           sep = ';', low_memory=False)
 
-#Quais deputados/as mais apresentaram PLs?
+#Quais deputados mais apresentaram PLs?
 autores = proposicoes.groupby('nomeAutor').count()
 autores = pd.DataFrame(autores)
 autores = autores.sort_values(by='idProposicao', ascending = False)
 autores = autores.reset_index()
 
+#Levando dados do dataframe, pro Google sheets
+sheet_autores = planilha.get_worksheet(1)
+sheet_autores.update([autores.columns.values.tolist()] + autores.values.tolist())
+
 maior_autor = autores.iloc[0]['nomeAutor']
 
 menor_autor = autores.iloc[-1]['nomeAutor']
 
-#Quantos projetos foram apresentados em 2022?
-qtd_proposicoes = proposicoes['idProposicao'].count()
-
-#PLs por estado
-PLs_estados = proposicoes.groupby('siglaUFAutor')['idProposicao'].mean()
-PLs_estados = PLs_estados.sort_values(ascending=False)
-PLs_estados = pd.DataFrame(PLs_estados)
-PLs_estados = PLs_estados.reset_index()
-
-#mostrar estado que tem maior quantidade de PLs
-estado_Pls = PLs_estados.iloc[0]['siglaUFAutor']
-
-#Criando robô no Telegram***
-
-#Configurando acesso ao Telegram e Google sheets
-
-TELEGRAM_API_KEY = os.environ["TELEGRAM_API_KEY"]
-GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
-with open("credenciais.json", mode="w") as arquivo:
-  arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
-api = gspread.authorize(conta)
-planilha = api.open_by_key("1BTcO4G_FS1tp6_hRcPUk_4fts6ayt7Ms2cvYHsqD9nM")
-sheet = planilha.worksheet("dadosrobo")
-
 # Criando site
-  
+
 app = Flask(__name__)
 
 menu = """
@@ -122,30 +114,43 @@ def contato():
 
 @app.route("/telegram", methods=["POST"])
 def telegram_bot():
-  #extraindo dados para enviar mensagens
-  update = request.json
-  chat_id = update["message"]["chat"]["id"]
-  message = update["message"]["text"]
-  first_name = update["message"]["from"]["first_name"]
-  sender_id = update["message"]["from"]["id"]
-
-  if message == "oi":
-    texto_resposta = f"Olá! Seja bem-vinda(o) {first_name}! O que gostaria de saber? \n 1 - Gastos dos deputados Federais no último ano? \n 2 - Projetos de Lei apresentados na Câmara Federal no último ano?"
-  elif message == "/start":
-    texto_resposta = f"Olá! Seja bem-vinda(o) {first_name}! O que gostaria de saber? \n 1 - Gastos dos deputados Federais no último ano? \n 2 - Projetos de Lei apresentados na Câmara Federal no último ano?"
-  elif message =='1':
-    texto_resposta =f"{first_name}, No último ano, o total gasto pelos(as) deputados federais foi igual à R${gastos}. \n A média de gastos da cota parlamentar por deputado(a) foi de R${mediaBr}, o(a) deputado(a) que mais gastou foi {maiorgastador}, o(a) que menos gastou foi {menorgastador}."
-  elif message =='2':
-    texto_resposta =f"{first_name}, Foram apresentados {qtd_proposicoes} projetos de Lei na Câmara Federal no último ano. \n Em média, o estado que mais apresentou PLs foi {estado_Pls}, o(a) deputado(a) que mais apresentou PLs foi {maior_autor}, já o que menos apresentou foi {menor_autor}"
-  else:
-    texto_resposta = "Não entendi!"
+  try:
+    # extraindo dados para enviar mensagens
+    update = request.json
+    chat_id = update["message"]["chat"]["id"]
+    message = update["message"]["text"]
+    first_name = update["message"]["from"]["first_name"]
+    sender_id = update["message"]["from"]["id"]
     
-  nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
- 
-  resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  print(resposta.text)
-  return "ok"
-
-@app.route("/")
-def hello_world():
-   return "Olá, mundo!"
+    # atualiza planilha com mensagens
+    sheet.update('A:B', [[message]])
+    resultado = sheet.get('A:B')
+    mensagem = resultado[-1][-1]
+    
+    linha = None  # adiciona a definição inicial de 'linha'
+    mensagens = ['oi', 'Oi', 'Olá', 'olá', 'ola', 'iai', 'qual é', 'e aí', "/start"]
+    if mensagem in mensagens:
+       texto_resposta = f"Olá! Seja bem-vinda(o) {first_name}! Eu sou o robô de olho na Câmara, para saber o gasto e os Projetos de Lei de um(a) deputado(a) digite seu nome."
+    else:
+      linha = sheet_gastadores.find(mensagem).row
+      valores = sheet_gastadores.row_values(linha)
+      gastos = valores[2]
+      linha_dois = sheet_autores.find(mensagem).row
+      valores_dois = sheet_autores.row_values(linha_dois)
+      PLs = valores_dois[1]
+      for mensagem in resultado:
+         texto_resposta = f'{first_name} {mensagem} apresentou {PLs} e gastou {gastos} no último ano'
+    
+    nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+    resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+  
+  except Exception as e:
+     print(e)
+     texto_resposta = "Erro ao processar a mensagem"
+     nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
+     resposta = requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
+     print(resultado)
+     print(linha)
+     print(valores)
+     print(gastos)
+     print(PLs)
